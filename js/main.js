d5e73e2c405d36964e4346551630efb9753e1a2a
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-29 16:07:26 +0100

    init + bonus 1 e 2

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+	el: '#root',
+	data: {
+		items: [],
+		inputValue: '',
+		isActive: true
+	},
+	methods: {
+		addItem: function() {
+			if( this.inputValue !== "" ) {
+				this.items.push({
+					todo: this.inputValue, 
+					check: false
+				});
+				this.inputValue = "";
+			}
+		},
+		removeItem: function(index) {
+			this.items.splice(index, 1);
+		}
+	}
+});
